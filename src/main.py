e0de4792c17117f643fdf3a630a6e5ab60db6e04
Luis <luisbrandino.contato@gmail.com>
2020-06-07 13:25:52 -0300

    added remaining time

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from TeamScrap import TeamScrap
 from colorama import Fore, Style
-from datetime import datetime
+from datetime import datetime, timedelta
 from crypt import Crypt
 import ascii_art
 
@@ -20,12 +20,19 @@ def clear_token():
 	encrypted_token_file.write(''.encode())
 	encrypted_token_file.close()
 
-def parse_date_time(date_time):
+def parse_teams_date_time(date_time):
 	all_date_parsed = date_time.split('T')
 	date_parsed = all_date_parsed[0].split('-')
-	#hour_parsed = all_date_parsed[1].replace('Z', '')
+	hour_parsed = all_date_parsed[1].replace('Z', '').split(':')
 
-	return date_parsed
+	return [date_parsed, hour_parsed]
+
+def parse_date_time(date_time):
+	all_date_parsed = str(date_time).split(' ')
+	date_parsed = all_date_parsed[0].split('-')
+	hour_parsed = all_date_parsed[1].split(':')
+
+	return [date_parsed, hour_parsed]
 
 print(ascii_art.return_ascii())
 
@@ -90,30 +97,46 @@ except Exception as err:
 	print(f'{Fore.RED}Verifique se o token não expirou ou está correto{Style.RESET_ALL}\n')
 	clear_token()
 	exit()
+	
+overdue_assignments = 0
 
 print('=' * 100)
 for class_assignment in classes_assignments:
 	due_date_time = class_assignment['assignmentInfo']['dueDateTime']
-	date_parsed = parse_date_time(due_date_time)
+	[date_parsed, hours_parsed] = parse_teams_date_time(due_date_time)
 
 	date_init = datetime.now()
-	date_final = datetime(year=int(date_parsed[0]), month=int(date_parsed[1]), day=int(date_parsed[2]))
+	date_final = datetime(year=int(date_parsed[0]), month=int(date_parsed[1]), day=int(date_parsed[2]), hour=int(hours_parsed[0]), minute=int(hours_parsed[1]), second=int(hours_parsed[2]))
+
+	date_final -= timedelta(hours=3)
+
+	[final_date_parsed, final_hours_parsed] = parse_date_time(date_final)
 
 	remaining_date = date_final - date_init
-	remaining_days = str(remaining_date).split(' ')[0]
+
+	remaining_date_parsed = str(remaining_date).split(',')
+
+	if len(remaining_date_parsed) > 1:
+		remaining_days = int(remaining_date_parsed[0].strip().split(' ')[0])
+		remaining_hours = remaining_date_parsed[1].strip().split(':')
+	else:
+		remaining_hours = remaining_date_parsed[0].strip().split(':')
 
 	print('\n')
 	print(f'{Fore.GREEN}Matéria:{Style.RESET_ALL} {class_assignment["classInfo"][0]["name"]}')
 	print(f"{Fore.GREEN}Descrição:{Style.RESET_ALL} {class_assignment['assignmentInfo']['displayName']}")
-	print(f'{Fore.GREEN}Data de entrega:{Style.RESET_ALL} {date_parsed[2]}/{date_parsed[1]}/{date_parsed[0]}')
-
-	if int(remaining_days) <= 0:
+	print(f'{Fore.GREEN}Data de entrega:{Style.RESET_ALL} {final_date_parsed[2]}/{final_date_parsed[1]}/{final_date_parsed[0]} às {final_hours_parsed[0]}:{final_hours_parsed[1]}:{final_hours_parsed[2]}')
+	
+	if len(remaining_date_parsed) == 1:
+		print(f'{Fore.GREEN}Tempo restante:{Style.RESET_ALL} {remaining_hours[0]} horas {remaining_hours[1]} minutos e {int(float(remaining_hours[2]))} segundos')
+	elif int(remaining_days) < 0:
 		print(f'{Fore.RED}Assignment em atraso!{Style.RESET_ALL}')
+		overdue_assignments += 1
 	else:
-		print(f'{Fore.GREEN}Dias restantes:{Style.RESET_ALL} {remaining_days}')
-
+		print(f'{Fore.GREEN}Tempo restante:{Style.RESET_ALL} {remaining_days} dias, {remaining_hours[0]} horas, {remaining_hours[1]} minutos e {int(float(remaining_hours[2]))} segundos')
 	print('\n')
 	print('=' * 100)
 
 print('\n')
 print(f'{Fore.GREEN}Total de assignments:{Style.RESET_ALL} {len(classes_assignments)}')
+print(f'{Fore.RED}Total de assignments em atraso:{Style.RESET_ALL} {overdue_assignments}')
